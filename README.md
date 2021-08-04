e6714c60e148499f746ec872b55fb4714107c5a4
Ben <bdl1@queensu.ca>
2021-08-04 15:44:18 -0400

    Added Content and Packages

---
+++ README.md
@@ -0,0 +1,9 @@
+# Usage
+
+```
+npx benlammers
+```
+
+All credit for this idea goes to: https://github.com/bnb/bitandbang
+  
+I saw it first here: https://github.com/bradwestfall/npx-bradwestfall
---
+++ bin/card.js
@@ -0,0 +1,48 @@
+#!/usr/bin/env node
+
+// All credit for this idea goes to: https://github.com/bnb/bitandbang
+
+import chalk from "chalk";
+import boxen from "boxen";
+
+// Boxen options
+const options = {
+   padding: 1,
+   margin: 1,
+   borderStyle: "singleDouble",
+   borderColor: "white",
+};
+
+// Text + chalk definitions
+const data = {
+   personal: chalk.cyan.bold("http://benlammers.dev"),
+   email: chalk.white.bold("21benlammers@gmail.com"),
+   github: chalk.gray.bold("https://github.com/") + chalk.green.bold("benlammers"),
+   twitter: chalk.gray.bold("https://twitter.com/") + chalk.red.bold("benlammers_"),
+   linkedin: chalk.gray.bold("https://www.linkedin.com/in/") + chalk.blue.bold("ben-lammers-3bb8bb168"),
+   card: chalk.white.bold("https://github.com/benlammers/npx-benlammers"),
+
+   subheading: chalk.white("Full Stack Web Developer"),
+
+   labelPersonal: chalk.white.underline.bold("Ben Lammers"),
+   labelEmail: chalk.cyan.bold("      Email:"),
+   labelGitHub: chalk.cyan.bold("     GitHub:"),
+   labelTwitter: chalk.cyan.bold("    Twitter:"),
+   labelLinkedIn: chalk.cyan.bold("   LinkedIn:"),
+   labelCard: chalk.cyan.bold("       Card:"),
+};
+
+// Actual strings we're going to output
+const heading = `              ${data.labelPersonal}  ${data.personal}`;
+const subheading = `                   ${data.subheading}`;
+
+const email = `${data.labelEmail}  ${data.email}`;
+const github = `${data.labelGitHub}  ${data.github}`;
+const twitter = `${data.labelTwitter}  ${data.twitter}`;
+const linkedin = `${data.labelLinkedIn}  ${data.linkedin}`;
+const card = `${data.labelCard}  ${data.card}`;
+
+// Put all our output together into a single variable so we can use boxen effectively
+const output = heading + "\n\n" + subheading + "\n\n\n" + email + "\n" + github + "\n" + twitter + "\n" + linkedin + "\n\n" + card;
+
+console.log(chalk.green(boxen(output, options)));
